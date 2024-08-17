ace17313411d70d8545d4104c147cf22dbb00e64
milimk <milimkim@gmail.com>
2024-08-17 00:49:07 -0700

    AI API connected in JS

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+function showPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 10,
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "Generating poem...",
-    autoStart: true,
-    delay: 20,
-  });
+  let instructionInput = document.querySelector("#user-instruction");
+  let context =
+    "You are a romantic poem and love to write poems. Make sure to follow the user instruction below and generate a poem that is not longer than couple paragraphs. Please separate each line with <br />. Sign the poem at the end with '-SheCodes AI'";
+  let prompt = `User instruction: Generate a poem about ${instructionInput.value}`;
+  let api_key = "0af4f7ebo6ce11605e35ecb7eatc1716";
+  let api_url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${api_key}`;
+
+  axios.get(api_url).then(showPoem);
 }
 
 let formElement = document.querySelector("#poem-generator-form");
